91db185f3c6c70fbc3b5e07bea8987f2fde9e41a
Helio <heliohenrique3@gmail.com>
2014-11-26 17:04:55 -0200

    Testes adicionais criados por Daniel.

--- src/poker/test/tests.py
+++ src/poker/test/tests.py
@@ -5,7 +5,8 @@ Created on 24/11/2014
 '''
 import unittest
 from poker.poker import poker, rank_mao, straigth
-from poker.dados import straight_flush2, quadra1, full_house1
+from poker.dados import straight_flush2, quadra1, full_house1, flush1, straigh1,\
+    um_par1, doi_pares1, trinca1, carta_alta1
 
 
 class Test(unittest.TestCase):
@@ -16,6 +17,16 @@ class Test(unittest.TestCase):
         self.assertEqual(poker([full_house1, quadra1]), quadra1)
         self.assertEqual(poker([straight_flush2, full_house1]), straight_flush2)
         #self.assertEqual(poker([straight_flush2, straight_flush2]), None)
+        
+        self.assertEqual(poker[(flush1, straigh1)], flush1)
+        self.assertEqual(poker[(straigh1, um_par1)], straigh1)
+        self.assertEqual(poker[(trinca1, doi_pares1)], trinca1)
+        self.assertEqual(poker[(um_par1, doi_pares1)], doi_pares1)
+        self.assertEqual(poker[(doi_pares1, flush1)], flush1)
+        self.assertEqual(poker[(carta_alta1, um_par1)], um_par1)
+        self.assertEqual(poker[(um_par1, carta_alta1)], um_par1)
+        self.assertEqual(poker[(straight_flush2, trinca1)], straight_flush2)
+        self.assertEqual(poker[(straight_flush2, carta_alta1)], straight_flush2)
          
     def testRank_mao(self):
         self.assertEqual(rank_mao(straight_flush2), 9)
